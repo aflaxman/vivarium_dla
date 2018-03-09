38f30b7ef94932a8f8262672daa04f39ef1634f7
A. Flaxman <abie@alum.mit.edu>
2018-03-09 10:30:08 -0800

    ENH: add config values

--- vivarium_dla/components.py
+++ vivarium_dla/components.py
@@ -1,8 +1,17 @@
 import numpy as np, pandas as pd, sklearn.neighbors
 
 class DLA:
+    configuration_defaults = {
+        'lda': {
+            'stickiness': 0.9,
+            'initial_position_sd': 100,
+            'step_sd': .1,
+            'near_radius': 1,
+        }
+    }
+
     def setup(self, builder):
-        self.stickiness = .1  # TODO: use config
+        self.config = builder.configuration.lda
         
         columns = ['x', 'y', 'frozen']
         self.population_view = builder.population.get_view(columns)
@@ -16,12 +25,13 @@ class DLA:
         """
         pop = pd.DataFrame(index=simulant_data.index)
         
-        pop['x'] = np.random.normal(scale=100, size=len(pop))
-        pop['y'] = np.random.normal(scale=100, size=len(pop))
+        pop['x'] = np.random.normal(scale=self.config.initial_position_sd, size=len(pop))
+        pop['y'] = np.random.normal(scale=self.config.initial_position_sd, size=len(pop))
 
         pop['frozen'] = False
 
-        pop.loc[0, :] = [0, 0, True]
+        # freeze first simulant in the batch 
+        pop.iloc[0, :] = [0, 0, True]
         
         # update the population in the model
         self.population_view.update(pop)
@@ -30,12 +40,12 @@ class DLA:
         pop = self.population_view.get(event.index)
         
         # move the not-frozen
-        pop.x += np.where(~pop.frozen, np.random.normal(scale=.1, size=len(pop)), 0)
-        pop.y += np.where(~pop.frozen, np.random.normal(scale=.1, size=len(pop)), 0)
+        pop.x += np.where(~pop.frozen, np.random.normal(scale=self.config.step_sd, size=len(pop)), 0)
+        pop.y += np.where(~pop.frozen, np.random.normal(scale=self.config.step_sd, size=len(pop)), 0)
         
         # freeze
         to_freeze = self.near_frozen(pop)
-        pop.loc[to_freeze, 'frozen'] = (np.random.normal(size=len(to_freeze)) < self.stickiness)
+        pop.loc[to_freeze, 'frozen'] = (np.random.normal(size=len(to_freeze)) < self.config.stickiness)
         self.population_view.update(pop)
         
     def near_frozen(self, pop):
@@ -43,7 +53,7 @@ class DLA:
         tree = sklearn.neighbors.KDTree(X, leaf_size=2)
         
         not_frozen = pop[~pop.frozen].loc[:, ['x', 'y']]
-        num_near = tree.query_radius(not_frozen.values, r=0.1, count_only=True)
+        num_near = tree.query_radius(not_frozen.values, r=self.config.near_radius, count_only=True)
         to_freeze = not_frozen[(num_near > 0)].index
         
         return to_freeze
